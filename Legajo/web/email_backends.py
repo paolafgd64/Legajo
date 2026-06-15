c18321bb2ae36bce0532752de940bdc1b26956eb
Paolaaa <paolafgd64@gmail.com>
2026-06-15 12:59:57 -0500

    Support IPv4 SMTP SSL

--- Legajo/web/email_backends.py
+++ Legajo/web/email_backends.py
@@ -2,11 +2,12 @@
 
 import socket
 import smtplib
+from django.core.mail.backends.smtp import DNS_NAME
 
 from django.core.mail.backends.smtp import EmailBackend
 
 
-class IPv4SMTP(smtplib.SMTP):
+class IPv4SocketMixin:
     """SMTP client that avoids IPv6 routes unavailable in some hosts."""
 
     def _get_socket(self, host, port, timeout):
@@ -39,5 +40,34 @@ class IPv4SMTP(smtplib.SMTP):
         raise OSError(f'Could not resolve an IPv4 address for {host}')
 
 
+class IPv4SMTP(IPv4SocketMixin, smtplib.SMTP):
+    pass
+
+
+class IPv4SMTP_SSL(IPv4SocketMixin, smtplib.SMTP_SSL):
+    pass
+
+
 class IPv4SMTPEmailBackend(EmailBackend):
-    connection_class = IPv4SMTP
+    def open(self):
+        if self.connection:
+            return False
+
+        connection_params = {'local_hostname': DNS_NAME.get_fqdn()}
+        if self.timeout is not None:
+            connection_params['timeout'] = self.timeout
+        if self.use_ssl:
+            connection_params['context'] = self.ssl_context
+
+        connection_class = IPv4SMTP_SSL if self.use_ssl else IPv4SMTP
+
+        try:
+            self.connection = connection_class(self.host, self.port, **connection_params)
+            if not self.use_ssl and self.use_tls:
+                self.connection.starttls(context=self.ssl_context)
+            if self.username and self.password:
+                self.connection.login(self.username, self.password)
+            return True
+        except OSError:
+            if not self.fail_silently:
+                raise
